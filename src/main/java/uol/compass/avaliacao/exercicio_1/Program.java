ac73af129fd696a6ce3833e9e7ef94b24b7ed3eb
Mateus de Oliveira e Silveira <svrmateus@gmail.com>
2021-11-26 14:52:58 -0300

    Primeira versão do menu de opções criada

--- src/main/java/uol/compass/avaliacao/exercicio_1/Program.java
+++ src/main/java/uol/compass/avaliacao/exercicio_1/Program.java
@@ -1,4 +1,53 @@
 package uol.compass.avaliacao.exercicio_1;
 
+import jdk.swing.interop.SwingInterOpUtils;
+import uol.compass.avaliacao.exercicio_1.controller.ProdutoController;
+import uol.compass.avaliacao.exercicio_1.model.Produto;
+
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ProdutoController produtoController = new ProdutoController();
+        Integer choice;
+
+        do {
+            System.out.println("Menu:");
+            System.out.println("1 - Cadastrar três produtos");
+            System.out.println("2 - Atualizar o primeiro produto");
+            System.out.println("3 - Excluir o segundo produto");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    produtoController.create(new Produto("Lápis", "Lápis de escrever", 10, 0.85));
+                    produtoController.create(new Produto("Borracha", "Borracha escolar", 20, 0.90));
+                    produtoController.create(new Produto("Caneta", "Caneta azul", 30, 0.87));
+                    break;
+                case 2:
+                    Produto produto = new Produto("Corretivo", "Corretivo de fita", 50, 11.61);
+                    produtoController.update(produto, 1);
+                    break;
+                case 3:
+                    produtoController.remove(2);
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Essa opção não existe! Tente novamente.");
+            }
+
+            System.out.println("\nSituação atual:");
+            produtoController.getList();
+            System.out.println();
+        } while (choice != 0);
+
+        System.out.println("Você saiu da aplicação. Até logo! =]");
+        sc.close();
+    }
 }
--- src/main/java/uol/compass/avaliacao/exercicio_1/model/Produto.java
+++ src/main/java/uol/compass/avaliacao/exercicio_1/model/Produto.java
@@ -8,6 +8,13 @@ public class Produto {
     private Integer quantidade;
     private Double preco;
 
+    public Produto(String nome, String descricao, Integer quantidade, Double preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
     public Produto(Integer id, String nome, String descricao, Integer quantidade, Double preco) {
         this.id = id;
         this.nome = nome;
