c665a2be1e889001b83594d0dab54fa1d2295598
Mateus de Oliveira e Silveira <svrmateus@gmail.com>
2021-11-26 11:17:31 -0300

    Entidade Produto criada

---
+++ src/main/java/uol/compass/avaliacao/exercicio_1/model/Produto.java
@@ -0,0 +1,10 @@
+package uol.compass.avaliacao.exercicio_1.model;
+
+public class Produto {
+
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private Integer quantidade;
+    private Double preco;
+}
