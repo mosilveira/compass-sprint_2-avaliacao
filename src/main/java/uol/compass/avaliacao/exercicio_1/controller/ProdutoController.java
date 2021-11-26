9067d5b46ed8185834b217df5306bd93e8bb10f6
Mateus de Oliveira e Silveira <svrmateus@gmail.com>
2021-11-26 13:37:36 -0300

    Camada controller criada

---
+++ src/main/java/uol/compass/avaliacao/exercicio_1/controller/ProdutoController.java
@@ -0,0 +1,34 @@
+package uol.compass.avaliacao.exercicio_1.controller;
+
+import uol.compass.avaliacao.exercicio_1.dao.ProdutoDAO;
+import uol.compass.avaliacao.exercicio_1.factory.ConnectionFactory;
+import uol.compass.avaliacao.exercicio_1.model.Produto;
+
+import java.sql.Connection;
+
+// Camada intermediária para as requisições
+public class ProdutoController {
+
+    ProdutoDAO produtoDAO;
+
+    public ProdutoController() {
+        Connection connection = new ConnectionFactory().getConnection();
+        this.produtoDAO = new ProdutoDAO(connection);
+    }
+
+    public void getList() {
+        this.produtoDAO.getList();
+    }
+
+    public void create(Produto produto) {
+        this.produtoDAO.create(produto);
+    }
+
+    public void remove(Integer id) {
+        this.produtoDAO.remove(id);
+    }
+
+    public void update(Produto produto, Integer id) {
+        this.produtoDAO.update(produto, id);
+    }
+}
